52d3e1560b15f168fe0846e1dc127bc017d2a94c
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-03 16:35:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
 	end
 		get '/date' do
 		  time = Time.new
-		  time = time.strftime("%A%B%d/%m/%Y")
+		  time = time.strftime("%A, %d/%m/%Y")
 		  split_time = time.split("/")
 		  @month = split_time[0].strftime("%B")
 		  binding.pry 
