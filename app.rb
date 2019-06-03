657b756188e6ab6afec343d3634b39b4f9d8a012
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-03 16:34:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
+require 'pry'
+
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+	get '/goodbye' do
+	 
+		erb :goodbye 
+	end
+		get '/hello' do
+		erb :hello 
+	end
+		get '/date' do
+		  time = Time.new
+		  time = time.strftime "%A%B%d/%m/%Y"
+		  split_time = time.split("/")
+		  @month = split_time[0].strftime("%B")
+		  binding.pry 
+		  
+		erb :date 
 	end
 
 
